2107b330ebb033c7727310412468f0ed8764cc76
Isamu Umetsu <isamu.umetsu@avinton.com>
2019-11-27 18:41:08 +0900

    formatted sql

--- sql/bat_last_id_info.sql
+++ sql/bat_last_id_info.sql
@@ -1,8 +1,19 @@
-CREATE TABLE _bat_last_id_info SELECT MAX(id) AS last_count,
-    order_overview_id,
-    ining,
-    top_bottom,
-    batter FROM
-    baseball.game_info
-GROUP BY batter , ining , top_bottom , order_overview_id
-ORDER BY order_overview_id , batter , ining , top_bottom
+CREATE TABLE _bat_last_id_info
+SELECT
+  MAX(id) AS last_count,
+  order_overview_id,
+  ining,
+  top_bottom,
+  batter
+FROM
+  baseball.game_info
+GROUP BY
+  batter,
+  ining,
+  top_bottom,
+  order_overview_id
+ORDER BY
+  order_overview_id,
+  batter,
+  ining,
+  top_bottom
