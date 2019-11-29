ff8f2e33dc22d909f7fcf02b171a26d1355b2c4c
Isamu Umetsu <isamu.umetsu@avinton.com>
2019-11-29 16:36:31 +0900

    delete unuse script

--- sql/bat_all_info.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    order_overview_id, ining, top_bottom, batter
-FROM
-    baseball.game_info
-GROUP BY batter , order_overview_id , ining , top_bottom
-ORDER BY order_overview_id , batter , ining , top_bottom
-;
--- sql/bat_last_id_info.sql
+++ sql/bat_last_id_info.sql
@@ -1,9 +1,6 @@
 -- CREATE TABLE _bat_last_id_info 
 -- insert into _bat_last_id_info (last_count, order_overview_id, ining, top_bottom, batter, batter_cnt) 
 
--- /*
-
--- 改良案
 SELECT
   g.id AS last_count,
   g.order_overview_id,
@@ -15,38 +12,10 @@ FROM
   baseball._game_info_specific g
   LEFT JOIN R_info r ON g.id = r.game_info_id
 WHERE
-  r.is_commit = 1
+  r.is_commit = 1 -- 結果が確定した打席のみ対象
 ORDER BY
   order_overview_id,
   batter,
   ining,
   top_bottom,
   batter_cnt;
-
--- */
-
-/*
-SELECT
-  MAX(id) AS last_count,
-  order_overview_id,
-  ining,
-  top_bottom,
-  batter,
-  batter_cnt
-FROM
-  baseball._game_info_specific
--- where order_overview_id = 63 and ining = 10 and top_bottom = 1 -- 打者１巡の試合
-GROUP BY
-  batter,
-  ining,
-  top_bottom,
-  order_overview_id,
-  batter_cnt
-ORDER BY
-  order_overview_id,
-  batter,
-  ining,
-  top_bottom,
-  batter_cnt
-;
-*/
