5bbebd5714d7b3954b7e4d869b62440634f3f3f8
Isamu Umetsu <isamu.umetsu@avinton.com>
2019-12-02 11:42:46 +0900

    update `game_info_specific`

--- sql/game_info_specific.sql
+++ sql/game_info_specific.sql
@@ -2,12 +2,13 @@
 -- set @prevbatter:=null;
 
 -- /*
-CREATE TABLE _game_info_specific
+-- CREATE TABLE _game_info_specific
 SELECT
   id AS game_info_id,
   order_overview_id,
   ining,
   top_bottom,
+  pitcher,
   batter,
   batter_cnt
 FROM
@@ -17,6 +18,7 @@ FROM
       g.order_overview_id,
       g.ining,
       g.top_bottom,
+      g.pitcher,
       g.batter,
       IF(@prevbatter = g.batter, @no, @no := @no + 1) AS batter_cnt,
       @prevbatter := g.batter
