5d3badbeed581fc900773bd2b4cc2d98f8cfa042
Isamu Umetsu <isamu.umetsu@avinton.com>
2019-12-03 11:05:17 +0900

    formatted query

--- query.js
+++ query.js
@@ -7,86 +7,88 @@ const query = (module.exports = {});
  */
 query.judgePlayerChange = (order_overview_id, now_pitch_count, top_bottom) => `
   SELECT
-  *
-  FROM
-  (
-    -- 選手交代前の前
-  SELECT
     *
   FROM
-  (
     (
+      -- 選手交代前の前
+      SELECT
+        *
+      FROM
+        (
+          (
             SELECT
-                pitch_count AS before_pitch_count,
-                batting_order AS before_batting_order,
-                player AS before_player,
-                pos AS before_pos,
-                profile_number AS before_profile_number,
-                player_name AS before_player_name
+              pitch_count AS before_pitch_count,
+              batting_order AS before_batting_order,
+              player AS before_player,
+              pos AS before_pos,
+              profile_number AS before_profile_number,
+              player_name AS before_player_name
             FROM
-                baseball.order_detail
+              baseball.order_detail
             WHERE
-                order_overview_id = ${order_overview_id} AND pitch_count = ${now_pitch_count -
-  1} AND top_bottom = ${top_bottom}
-        ) AS A
-        LEFT OUTER JOIN
-            (
-                SELECT
-                    pitch_count AS after_pitch_count,
-                    batting_order AS after_batting_order,
-                    player AS after_player,
-                    pos AS after_pos,
-                    profile_number AS after_profile_number,
-                    player_name AS after_player_name
-                FROM
-                    baseball.order_detail
-                WHERE
-                    order_overview_id = ${order_overview_id} AND pitch_count = ${now_pitch_count} AND top_bottom = ${top_bottom}
-            ) AS B
-        ON  A.before_player = B.after_player AND
-            A.before_pos = B.after_pos
-  )
-  UNION
-    -- 選手交代前の後
-  SELECT
-    *
-  FROM
-  (
-    (
+              order_overview_id = ${order_overview_id}
+              AND pitch_count = ${now_pitch_count - 1}
+              AND top_bottom = ${top_bottom}
+          ) AS A
+          LEFT OUTER JOIN (
             SELECT
-                pitch_count AS before_pitch_count,
-                batting_order AS before_batting_order,
-                player AS before_player,
-                pos AS before_pos,
-                profile_number AS before_profile_number,
-                player_name AS before_player_name
+              pitch_count AS after_pitch_count,
+              batting_order AS after_batting_order,
+              player AS after_player,
+              pos AS after_pos,
+              profile_number AS after_profile_number,
+              player_name AS after_player_name
             FROM
-                baseball.order_detail
+              baseball.order_detail
             WHERE
-                order_overview_id = ${order_overview_id} AND pitch_count = ${now_pitch_count -
-  1} AND top_bottom = ${top_bottom}
-        ) AS A
-        RIGHT OUTER JOIN
-    (
+              order_overview_id = ${order_overview_id}
+              AND pitch_count = ${now_pitch_count}
+              AND top_bottom = ${top_bottom}
+          ) AS B ON A.before_player = B.after_player
+          AND A.before_pos = B.after_pos
+        )
+      UNION
+      -- 選手交代前の後
+      SELECT
+        *
+      FROM
+        (
+          (
             SELECT
-                pitch_count AS after_pitch_count,
-                batting_order AS after_batting_order,
-                player AS after_player,
-                pos AS after_pos,
-                profile_number AS after_profile_number,
-                player_name AS after_player_name
+              pitch_count AS before_pitch_count,
+              batting_order AS before_batting_order,
+              player AS before_player,
+              pos AS before_pos,
+              profile_number AS before_profile_number,
+              player_name AS before_player_name
             FROM
-                baseball.order_detail
+              baseball.order_detail
             WHERE
-                order_overview_id = ${order_overview_id} AND pitch_count = ${now_pitch_count} AND top_bottom = ${top_bottom}
-        ) AS B
-        ON  A.before_player = B.after_player AND
-            A.before_pos = B.after_pos
-  )
-  ) AS C
+              order_overview_id = ${order_overview_id}
+              AND pitch_count = ${now_pitch_count - 1}
+              AND top_bottom = ${top_bottom}
+          ) AS A
+          RIGHT OUTER JOIN (
+            SELECT
+              pitch_count AS after_pitch_count,
+              batting_order AS after_batting_order,
+              player AS after_player,
+              pos AS after_pos,
+              profile_number AS after_profile_number,
+              player_name AS after_player_name
+            FROM
+              baseball.order_detail
+            WHERE
+              order_overview_id = ${order_overview_id}
+              AND pitch_count = ${now_pitch_count}
+              AND top_bottom = ${top_bottom}
+          ) AS B ON A.before_player = B.after_player
+          AND A.before_pos = B.after_pos
+        )
+    ) AS C
   WHERE
-  before_batting_order IS NULL OR
-    after_player IS NULL
+    before_batting_order IS NULL
+    OR after_player IS NULL
 `;
 
 /**
@@ -97,67 +99,74 @@ query.getGameInfoWhenChange = (
   before_pitch_count,
   after_pitch_count
 ) => `
-    SELECT
-        g.ining,
-        g.top_bottom,
-        pp.name AS pitcher,
-        pp.profile_number AS p_pn,
-        pb.name AS batter,
-        pb.profile_number AS b_pn,
-        g.strike,
-        g.ball,
-        g.out,
-        p1.name AS runner_1b,
-        p1.profile_number AS runner_1b_pn,
-        p2.name AS runner_2b,
-        p2.profile_number AS runner_2b_pn,
-        p3.name AS runner_3b,
-        p3.profile_number AS runner_3b_pn
-    FROM
-        baseball.game_info AS g
+  SELECT
+    g.ining,
+    g.top_bottom,
+    pp.name AS pitcher,
+    pp.profile_number AS p_pn,
+    pb.name AS batter,
+    pb.profile_number AS b_pn,
+    g.strike,
+    g.ball,
+    g.out,
+    p1.name AS runner_1b,
+    p1.profile_number AS runner_1b_pn,
+    p2.name AS runner_2b,
+    p2.profile_number AS runner_2b_pn,
+    p3.name AS runner_3b,
+    p3.profile_number AS runner_3b_pn
+  FROM
+    baseball.game_info AS g
     LEFT JOIN baseball.player p1 ON g.runner_1b = p1.id
     LEFT JOIN baseball.player p2 ON g.runner_2b = p2.id
     LEFT JOIN baseball.player p3 ON g.runner_3b = p3.id
     LEFT JOIN baseball.player pp ON g.pitcher = pp.id
     LEFT JOIN baseball.player pb ON g.batter = pb.id
-    WHERE g.order_overview_id = ${order_overview_id} AND g.pitch_count IN (${before_pitch_count}, ${after_pitch_count})
+  WHERE
+    g.order_overview_id = ${order_overview_id}
+    AND g.pitch_count IN (${before_pitch_count}, ${after_pitch_count})
 `;
 
 /**
  * 打順ごとのスタメン回数取得
  */
 query.getStartingMenberSpecifyOrder = (team, order, idsTop, idsBtm) => `
-    SELECT 
-        count(player_name) as count,
-        player_name
-    FROM
-        baseball.order_detail
-    where
-        ((
-            order_overview_id in (
-                ${idsTop}
-                -- SELECT 
-                --     id
-                -- FROM
-                --     baseball.order_overview
-                -- where
-                --     visitor_team = '${team}'
-            ) and top_bottom = 1
-        ) or (
-            order_overview_id in (
-                ${idsBtm}
-                -- SELECT 
-                --     id
-                -- FROM
-                --     baseball.order_overview
-                -- where
-                --     home_team = '${team}'
-            ) and top_bottom = 2
-        ))
-        and pitch_count = 1
-        and batting_order = ${order}
-    group by player_name
-    order by count desc
+  SELECT
+    count(player_name) as count,
+    player_name
+  FROM
+    baseball.order_detail
+  where
+    (
+      (
+        order_overview_id in (
+          ${idsTop} -- SELECT 
+          --     id
+          -- FROM
+          --     baseball.order_overview
+          -- where
+          --     visitor_team = '${team}'
+        )
+        and top_bottom = 1
+      )
+      or (
+        order_overview_id in (
+          ${idsBtm} -- SELECT 
+          --     id
+          -- FROM
+          --     baseball.order_overview
+          -- where
+          --     home_team = '${team}'
+        )
+        and top_bottom = 2
+      )
+    )
+    and pitch_count = 1
+    and batting_order = ${order}
+  group by
+    player_name
+  order by
+    count desc
 `;
 
 query.getOverviewIds = (team, top_bottom) => `
@@ -197,8 +206,8 @@ const getFullParticipationBySide = (top_bottom, order, ids) => `
     baseball.order_detail
   WHERE
     pitch_count = 1
-    AND batting_order = $ { order }
-    AND top_bottom = $ { top_bottom }
+    AND batting_order = ${order}
+    AND top_bottom = ${top_bottom}
     AND order_overview_id IN (
       SELECT
         B.order_overview_id
@@ -218,9 +227,9 @@ const getFullParticipationBySide = (top_bottom, order, ids) => `
               FROM
                 baseball.order_detail
               WHERE
-                order_overview_id IN ($ { ids })
-                AND top_bottom = $ { top_bottom }
-                AND batting_order = $ { order }
+                order_overview_id IN (${ids})
+                AND top_bottom = ${top_bottom}
+                AND batting_order = ${order}
               GROUP BY
                 order_overview_id,
                 top_bottom,
@@ -445,46 +454,77 @@ const averageByBat = (bat, limitPA, tableName) => {
   const colPA = `pa${bat}`, colAB = `ab${bat}`, colCnt = `cnt${bat}`, colRate = `rate${bat}`;
 
   return `
-    SELECT 
-      h.id, h.name, h.team, h.${colAB} AS bat_cnt, h.${colCnt} AS target_cnt, h.${colRate} AS average, rank.rank
+    SELECT
+      h.id,
+      h.name,
+      h.team,
+      h.${colAB} AS bat_cnt,
+      h.${colCnt} AS target_cnt,
+      h.${colRate} AS average,
+      rank.rank
     FROM
       baseball.${tableName} h
-    LEFT JOIN
-      (SELECT 
-        id, score, rank
-      FROM 
-        (SELECT 
-          score, @rank AS rank, cnt, @rank:=@rank + cnt
+      LEFT JOIN (
+        SELECT
+          id,
+          score,
+          rank
         FROM
-          (SELECT @rank:=1) AS Dummy,
-          (SELECT 
-            ${colRate} AS score, COUNT(*) AS cnt
-          FROM
-            (SELECT 
-              id, name, team, ${colAB}, ${colCnt}, ${colRate}
+          (
+            SELECT
+              score,
+              @rank AS rank,
+              cnt,
+              @rank := @rank + cnt
+            FROM
+              (
+                SELECT
+                  @rank := 1
+              ) AS Dummy,
+              (
+                SELECT
+                  ${colRate} AS score,
+                  COUNT(*) AS cnt
+                FROM
+                  (
+                    SELECT
+                      id,
+                      name,
+                      team,
+                      ${colAB},
+                      ${colCnt},
+                      ${colRate}
+                    FROM
+                      ${tableName}
+                    WHERE
+                      ${colPA} >= ${limitPA}
+                  ) AS htb
+                GROUP BY
+                  score
+                ORDER BY
+                  score DESC
+              ) AS GroupBy
+          ) AS Ranking
+          JOIN (
+            SELECT
+              id,
+              name,
+              team,
+              ${colAB},
+              ${colCnt},
+              ${colRate}
             FROM
               ${tableName}
             WHERE
               ${colPA} >= ${limitPA}
-            ) AS htb
-          GROUP BY score
-          ORDER BY score DESC
-          ) AS GroupBy
-        ) AS Ranking
-      JOIN
-        (SELECT 
-          id, name, team, ${colAB}, ${colCnt}, ${colRate}
-        FROM
-          ${tableName}
-        WHERE
-          ${colPA} >= ${limitPA}
-        ) AS htb ON htb.${colRate} = Ranking.score
-      ORDER BY rank ASC
-      ) AS rank
-    ON rank.id = h.id
+          ) AS htb ON htb.${colRate} = Ranking.score
+        ORDER BY
+          rank ASC
+      ) AS rank ON rank.id = h.id
     WHERE
       h.${colPA} >= ${limitPA}
-    ORDER BY h.${colRate} DESC
+    ORDER BY
+      h.${colRate} DESC
   `
 }
 
@@ -515,49 +555,86 @@ query.averageOpsBat = (bat, limitPA) => {
   const rate = `rate${bat}`
   const pa = `pa${bat}`
   return `
-    SELECT 
-      o.id, o.name, o.team, o.rate + s.rate AS rate_sum, o.${rate} + s.${rate} AS rate, o.${rate} AS onbase, s.${rate} AS slugging, o.${pa} AS pa, rank.rank
+    SELECT
+      o.id,
+      o.name,
+      o.team,
+      o.rate + s.rate AS rate_sum,
+      o.${rate} + s.${rate} AS rate,
+      o.${rate} AS onbase,
+      s.${rate} AS slugging,
+      o.${pa} AS pa,
+      rank.rank
     FROM
       baseball.average_onbase_horizontal o
-    LEFT JOIN baseball.average_slugging_horizontal s ON o.batter = s.batter
-    LEFT JOIN
-      (SELECT 
-        id, score, rank
-      FROM 
-        (SELECT 
-          score, @rank AS rank, cnt, @rank:=@rank + cnt
+      LEFT JOIN baseball.average_slugging_horizontal s ON o.batter = s.batter
+      LEFT JOIN (
+        SELECT
+          id,
+          score,
+          rank
         FROM
-          (SELECT @rank:=1) AS Dummy,
-          (SELECT 
-            rate AS score, COUNT(*) AS cnt
-          FROM
-            (SELECT 
-              o.id, o.name, o.team, o.rate + s.rate AS rate_sum, o.${rate} + s.${rate} AS rate, o.${rate} AS onbase, s.${rate} AS slugging, o.${pa} AS pa
+          (
+            SELECT
+              score,
+              @rank AS rank,
+              cnt,
+              @rank := @rank + cnt
+            FROM
+              (
+                SELECT
+                  @rank := 1
+              ) AS Dummy,
+              (
+                SELECT
+                  rate AS score,
+                  COUNT(*) AS cnt
+                FROM
+                  (
+                    SELECT
+                      o.id,
+                      o.name,
+                      o.team,
+                      o.rate + s.rate AS rate_sum,
+                      o.${rate} + s.${rate} AS rate,
+                      o.${rate} AS onbase,
+                      s.${rate} AS slugging,
+                      o.${pa} AS pa
+                    FROM
+                      baseball.average_onbase_horizontal o
+                      LEFT JOIN baseball.average_slugging_horizontal s ON o.batter = s.batter
+                    WHERE
+                      o.${pa} >= ${limitPA}
+                  ) AS htb
+                GROUP BY
+                  score
+                ORDER BY
+                  score DESC
+              ) AS GroupBy
+          ) AS Ranking
+          JOIN (
+            SELECT
+              o.id,
+              o.name,
+              o.team,
+              o.rate + s.rate AS rate_sum,
+              o.${rate} + s.${rate} AS rate,
+              o.${rate} AS onbase,
+              s.${rate} AS slugging,
+              o.${pa} AS pa
             FROM
               baseball.average_onbase_horizontal o
-            LEFT JOIN baseball.average_slugging_horizontal s ON o.batter = s.batter
+              LEFT JOIN baseball.average_slugging_horizontal s ON o.batter = s.batter
             WHERE
               o.${pa} >= ${limitPA}
-            ) AS htb
-          GROUP BY score
-          ORDER BY score DESC
-          ) AS GroupBy
-        ) AS Ranking
-      JOIN
-        (SELECT 
-          o.id, o.name, o.team, o.rate + s.rate AS rate_sum, o.${rate} + s.${rate} AS rate, o.${rate} AS onbase, s.${rate} AS slugging, o.${pa} AS pa
-        FROM
-          baseball.average_onbase_horizontal o
-        LEFT JOIN baseball.average_slugging_horizontal s ON o.batter = s.batter
-        WHERE
-          o.${pa} >= ${limitPA}
-        ) AS htb ON htb.rate = Ranking.score
-      ORDER BY rank ASC
-      ) AS rank
-    ON rank.id = o.id
+          ) AS htb ON htb.rate = Ranking.score
+        ORDER BY
+          rank ASC
+      ) AS rank ON rank.id = o.id
     WHERE
       o.${pa} >= ${limitPA}
-    ORDER BY rate DESC
+    ORDER BY
+      rate DESC
 `}
 
 /**
