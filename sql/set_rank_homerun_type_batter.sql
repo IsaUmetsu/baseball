d66492c465323f1b3b0b1a21a77935b100095e85
Isamu Umetsu <isamu.umetsu@avinton.com>
2019-12-03 18:33:00 +0900

    formatted query

--- sql/set_rank_homerun_type_batter.sql
+++ sql/set_rank_homerun_type_batter.sql
@@ -1,8 +1,28 @@
+/**
+select * from
+(SELECT 
+    id, score, rank
+FROM
+    (SELECT 
+		score, @rank AS rank, cnt, @rank:=@rank + cnt
+    FROM
+        (SELECT @rank:=1) AS Dummy, (SELECT 
+        cnt as score, COUNT(*) AS cnt
+    FROM
+        (select * from hr_type_situation_b where homerun_type = '追い上げ') as htb
+    GROUP BY score
+    ORDER BY score DESC) AS GroupBy) AS Ranking
+        JOIN
+    (select * from hr_type_situation_b where homerun_type = '追い上げ') as htb ON htb.cnt = Ranking.score
+ORDER BY rank ASC
+) as rank;
+-- */
+
 -- /*
 SELECT 
-    h.id, h.summary, h.cnt, h.total_cnt, h.percent, rank.rank
+    h.id, h.name, h.team, h.cnt, h.batting_cnt, h.percent, rank.rank
 FROM
-    baseball.homerun_type_batter h
+    baseball.hr_type_situation_b h
         LEFT JOIN
     (SELECT 
         id, score, rank
@@ -16,7 +36,7 @@ FROM
         (SELECT 
         *
     FROM
-        homerun_type_batter
+        hr_type_situation_b
     WHERE
         homerun_type = '追い上げ') AS htb
     GROUP BY score
@@ -24,31 +44,11 @@ FROM
     JOIN (SELECT 
         *
     FROM
-        homerun_type_batter
+        hr_type_situation_b
     WHERE
         homerun_type = '追い上げ') AS htb ON htb.cnt = Ranking.score
     ORDER BY rank ASC) AS rank ON rank.id = h.id
 WHERE
     h.homerun_type = '追い上げ'
 ORDER BY rank.rank DESC;
--- */
-
-/**
-select * from
-(SELECT 
-    id, score, rank
-FROM
-    (SELECT 
-		score, @rank AS rank, cnt, @rank:=@rank + cnt
-    FROM
-        (SELECT @rank:=1) AS Dummy, (SELECT 
-        cnt as score, COUNT(*) AS cnt
-    FROM
-        (select * from homerun_type_batter where homerun_type = '追い上げ') as htb
-    GROUP BY score
-    ORDER BY score DESC) AS GroupBy) AS Ranking
-        JOIN
-    (select * from homerun_type_batter where homerun_type = '追い上げ') as htb ON htb.cnt = Ranking.score
-ORDER BY rank ASC
-) as rank;
 -- */
