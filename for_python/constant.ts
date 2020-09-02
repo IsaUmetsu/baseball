4b1651fb1cf3c45fed616167073b8adc2b679d22
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-09-02 16:02:58 +0900

    定数モジュール追加

---
+++ for_python/constant.ts
@@ -0,0 +1,44 @@
+export const teamArray = {
+  'H': 'ソ',
+  'M': 'ロ',
+  'E': '楽',
+  'F': '日',
+  'L': '西',
+  'B': 'オ',
+  'G': '巨',
+  'T': '神',
+  'De': 'デ',
+  'D': '中',
+  'C': '広',
+  'S': 'ヤ'
+}
+
+export const teamHashTags = {
+  'H': '#sbhawks',
+  'M': '#chibalotte',
+  'E': '#RakutenEagles',
+  'F': '#lovefigters',
+  'L': '#seibulions',
+  'B': '#bs2020',
+  'G': '#giants',
+  'T': '#hanshin',
+  'De': '#baysters',
+  'D': '#dragons',
+  'C': '#carp',
+  'S': '#swallows'
+}
+
+export const teamNames = {
+  'H': 'ソフトバンク',
+  'M': 'ロッテ',
+  'E': '楽天',
+  'F': '日本ハム',
+  'L': '西武',
+  'B': 'オリックス',
+  'G': '巨人',
+  'T': '阪神',
+  'De': 'DeNA',
+  'D': '中日',
+  'C': '広島',
+  'S': 'ヤクルト'
+}
