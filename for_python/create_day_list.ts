0470530cdd6943e6314949cc96d252cc91e3d065
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-09-02 12:04:47 +0900

    update daylist

--- for_python/create_day_list.ts
+++ for_python/create_day_list.ts
@@ -1,14 +1,26 @@
 import * as moment from 'moment';
+import { exit } from 'process';
 
-const targetDay = 2;
-const seasonStartDate = "2020-06-19";
+const targetDay = Number(process.env.d);
+if (! targetDay || targetDay < 0 || 7 < targetDay) {
+    console.log("0~7で入力してください。");
+    exit;
+}
 
-const seasonStart = moment(seasonStartDate);
-const seasonEnd = moment("2020-08-23");
-const startDate = moment(seasonStartDate);
+const seasonYear = "2020";
+const seasonStartDate = "0619";
+const dateFormat = "YYYYMMDD";
+
+const startDate = moment(seasonYear + seasonStartDate, dateFormat);
+const seasonStart = moment(seasonYear + seasonStartDate, dateFormat);
+
+if (process.env.se && process.env.se.length != 4) {
+  console.log('正しく日付指定してください。')
+  exit;
+}
+const seasonEnd = process.env.se ? moment(seasonYear + process.env.se, dateFormat) : moment();
 
 const dateList: string[] = [];
-
 while (startDate.isSameOrAfter(seasonStart) && startDate.isSameOrBefore(seasonEnd)) {
     if (startDate.day() == targetDay) dateList.push(startDate.format("\"YYYYMMDD\""));
     startDate.add(1, "days");
