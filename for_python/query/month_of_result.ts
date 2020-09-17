c8a6f1820dec7aa4ec6b68937a009ed79abaa151
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-09-17 20:15:58 +0900

    added script: result of month

---
+++ for_python/query/month_of_result.ts
@@ -0,0 +1,130 @@
+import { format } from 'util';
+import * as moment from 'moment';
+
+import { createConnection, getManager } from 'typeorm';
+import { teamHashTags, leagueList, dayOfWeekArr } from '../constant';
+import { checkArgLG, trimRateZero } from '../disp_util';
+
+// Execute
+(async () => {
+  await createConnection('default');
+
+  const league = process.env.LG;
+  const teams = checkArgLG(league);
+  if (! teams.length) return;
+
+  let monthArg = Number(process.env.M);
+  if (! monthArg) {
+    monthArg = Number(moment().format('M'));
+    console.log(format('M=[月] を指定がないため今月(%d月)のデータを出力します', monthArg));
+  } else if (monthArg < 6 || 12 < monthArg) {
+    console.log('M=[月] は6〜12月の間で入力してください');
+    return;
+  }
+
+  const firstDay = moment(format("2020%d", monthArg), "YYYYM").startOf('month').format('YYYYMMDD');
+  const lastDay = moment(format("2020%d", monthArg), "YYYYM").endOf('month').format('YYYYMMDD');
+
+  const manager = await getManager();
+  const results = await manager.query(`
+    SELECT
+      base.team_initial_kana,
+      base.team_initial,
+      base.game_cnt,
+      away.win_count_away + home.win_count_home AS win_count,
+      away.lose_count_away + home.lose_count_home AS lose_count,
+      away.draw_count_away + home.draw_count_home AS draw_count,
+      ROUND((away.win_count_away + home.win_count_home) / (base.game_cnt - (away.draw_count_away + home.draw_count_home)), 3) AS win_rate,
+      '' AS eol
+    FROM
+      game_cnt_per_month base
+      LEFT JOIN (
+          SELECT
+              away_initial AS team_initial,
+              COUNT(
+                  away_initial = CASE
+                      WHEN home_score > away_score THEN home_initial
+                      WHEN home_score < away_score THEN away_initial
+                      ELSE NULL
+                  END
+                  OR NULL
+              ) AS win_count_away,
+              COUNT(
+                  away_initial = CASE
+                      WHEN home_score < away_score THEN home_initial
+                      WHEN home_score > away_score THEN away_initial
+                      ELSE NULL
+                  END
+                  OR NULL
+              ) AS lose_count_away,
+              COUNT(
+                  away_initial = CASE
+                      WHEN home_score = away_score THEN away_initial
+                      ELSE NULL
+                  END
+                  OR NULL
+              ) AS draw_count_away,
+              eol
+          FROM
+              baseball_2020.debug_base
+          WHERE
+              no_game = 0
+              AND batting_result = '試合終了'
+              AND (date >= '${firstDay}' AND date <= '${lastDay}')
+          GROUP BY
+              away_initial
+      ) away ON away.team_initial = base.team_initial_kana
+      LEFT JOIN (
+          SELECT
+              home_initial AS team_initial,
+              COUNT(
+                  home_initial = CASE
+                      WHEN home_score > away_score THEN home_initial
+                      WHEN home_score < away_score THEN away_initial
+                      ELSE NULL
+                  END
+                  OR NULL
+              ) AS win_count_home,
+              COUNT(
+                  home_initial = CASE
+                      WHEN home_score < away_score THEN home_initial
+                      WHEN home_score > away_score THEN away_initial
+                      ELSE NULL
+                  END
+                  OR NULL
+              ) AS lose_count_home,
+              COUNT(
+                  home_initial = CASE
+                      WHEN home_score = away_score THEN home_initial
+                      ELSE NULL
+                  END
+                  OR NULL
+              ) AS draw_count_home,
+              eol
+          FROM
+              baseball_2020.debug_base
+          WHERE
+              no_game = 0
+              AND batting_result = '試合終了'
+              AND (date >= '${firstDay}' AND date <= '${lastDay}')
+          GROUP BY
+              home_initial
+      ) home ON home.team_initial = base.team_initial_kana
+    WHERE
+      base.month = ${monthArg} AND base.team_initial_kana IN (${teams.join(", ")})
+    ORDER BY
+      win_rate DESC
+  `);
+  
+  console.log(format("\n%s球団 %s月 成績\n", league ? leagueList[league] + '6' : 'NPB12', monthArg));
+  results.forEach(result => {
+    const { team_initial_kana, team_initial, win_count, lose_count, draw_count, win_rate } = result;
+
+    console.log(format(
+      "%s  %s勝%s敗%s %s %s ",
+      team_initial_kana, win_count, lose_count,
+      draw_count > 0 ? format("%s分", draw_count) : '',
+      trimRateZero(win_rate), teamHashTags[team_initial]
+    ));  
+  });
+})();
---
+++ for_python/query/month_of_result_average.ts
@@ -0,0 +1,56 @@
+import { format } from 'util';
+import * as moment from 'moment';
+
+import { createConnection, getManager } from 'typeorm';
+import { teamHashTags, leagueList, dayOfWeekArr, teamArray } from '../constant';
+import { checkArgLG, trimRateZero } from '../disp_util';
+
+// Execute
+(async () => {
+  await createConnection('default');
+
+  const league = process.env.LG;
+  const teams = checkArgLG(league);
+  if (! teams.length) return;
+
+  let monthArg = Number(process.env.M);
+  if (! monthArg) {
+    monthArg = Number(moment().format('M'));
+    console.log(format('M=[月] を指定がないため今月(%d月)のデータを出力します', monthArg));
+  } else if (monthArg < 6 || 12 < monthArg) {
+    console.log('M=[月] は6〜12月の間で入力してください');
+    return;
+  }
+
+  const firstDay = moment(format("2020%d", monthArg), "YYYYM").startOf('month').format('YYYYMMDD');
+  const lastDay = moment(format("2020%d", monthArg), "YYYYM").endOf('month').format('YYYYMMDD');
+
+  const manager = await getManager();
+  const results = await manager.query(`
+    SELECT 
+      b_team AS tm,
+      SUM(is_ab) AS bat,
+      SUM(is_hit) AS hit,
+      ROUND(SUM(is_hit) / SUM(is_ab), 3) AS average
+    FROM
+      baseball_2020.debug_base
+    WHERE
+      date BETWEEN '${firstDay}' AND '${lastDay}'
+      AND CHAR_LENGTH(b_team) > 0
+      AND b_team IN (${teams.join(', ')})
+    GROUP BY b_team
+    ORDER BY average DESC
+  `);
+  
+  console.log(format("\n%s球団 %s月 打率\n", league ? leagueList[league] + '6' : 'NPB12', monthArg));
+  results.forEach(result => {
+    const { tm, bat, hit, average } = result;
+
+    const [ team_initial, tm_init_kana ] = Object.entries(teamArray).find(([key, value]) => value == tm);
+
+    console.log(format(
+      "%s %s (%s-%s) %s ",
+      tm, trimRateZero(average), bat, hit, teamHashTags[team_initial]
+    ));  
+  });
+})();
