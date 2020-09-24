785b10a94d528bcd112e96ace7dbb4efc9833d88
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-09-24 13:45:23 +0900

    modefied major:pos

--- for_python/query/major_pos.ts
+++ for_python/query/major_pos.ts
@@ -1,7 +1,7 @@
 import { format } from "util";
 
 import { createConnection, getManager } from 'typeorm';
-import { leagueList, posFullDic } from '../constant';
+import { leagueList, posFullDic, posArgDic } from '../constant';
 import { checkArgPs, checkArgTMLG, displayResult, trimRateZero } from "../disp_util";
 import { getIsTweet, tweetMulti } from '../tweet/tw_util';
 
@@ -15,43 +15,32 @@ import { getIsTweet, tweetMulti } from '../tweet/tw_util';
   const teams = checkArgTMLG(teamArg, leagueArg);
   if (! teams.length) return;
 
-  const posArg = process.env.PS;
-  let pos = checkArgPs(posArg);
-  if (! pos) return;
+  let positions = [];
+  const pos = checkArgPs(process.env.PS);
+  if (! pos) {
+    positions = Object.values(posArgDic);
+  } else {
+    positions.push(pos);
+  }
 
   const manager = await getManager();
-  const results = await manager.query(`
-    SELECT
-      REPLACE(name, ' ', '') AS batter,
-      b_team AS tm,
-      SUM(ab) AS ab,
-      SUM(hit) AS hit,
-      ROUND(SUM(hit) / SUM(ab), 3) AS ave,
-      SUM(hr) AS hr,
-      SUM(rbi) AS rbi
-    FROM
-      baseball_2020.stats_batter sb
-    WHERE
-      (sb.name, b_team) IN (
-        SELECT
-          A.name, A.b_team
-        FROM (
+
+  for (const position of positions) {
+    const results = await manager.query(`
+      SELECT
+        REPLACE(name, ' ', '') AS batter,
+        b_team AS tm,
+        SUM(ab) AS ab,
+        SUM(hit) AS hit,
+        ROUND(SUM(hit) / SUM(ab), 3) AS ave,
+        SUM(hr) AS hr,
+        SUM(rbi) AS rbi
+      FROM
+        baseball_2020.stats_batter sb
+      WHERE
+        (sb.name, b_team) IN (
           SELECT
-            name,
-            b_team,
-            SUM(ab) AS ab
-          FROM
-            baseball_2020.stats_batter sb
-          WHERE
-            position LIKE '%${pos}%'
-          GROUP BY
-            name,
-            b_team
-        ) AS A
-        LEFT JOIN (
-          SELECT
-            b_team,
-            MAX(ab) AS ab
+            A.name, A.b_team
           FROM (
             SELECT
               name,
@@ -60,39 +49,59 @@ import { getIsTweet, tweetMulti } from '../tweet/tw_util';
             FROM
               baseball_2020.stats_batter sb
             WHERE
-              position LIKE '%${pos}%'
+              position LIKE '%${position}%'
             GROUP BY
               name,
               b_team
-            ) AS A
-          GROUP BY b_team
-        ) AS B ON A.b_team = B.b_team AND A.ab = B.ab
-        WHERE B.b_team IS NOT NULL
-      )
-      AND b_team IN (${teams.join(',')})
-    GROUP BY
-      name,
-      b_team
-    HAVING
-      ave IS NOT NULL
-    ORDER BY
-      ave DESC
-  `);
+          ) AS A
+          LEFT JOIN (
+            SELECT
+              b_team,
+              MAX(ab) AS ab
+            FROM (
+              SELECT
+                name,
+                b_team,
+                SUM(ab) AS ab
+              FROM
+                baseball_2020.stats_batter sb
+              WHERE
+                position LIKE '%${position}%'
+              GROUP BY
+                name,
+                b_team
+              ) AS A
+            GROUP BY b_team
+          ) AS B ON A.b_team = B.b_team AND A.ab = B.ab
+          WHERE B.b_team IS NOT NULL
+        )
+        AND b_team IN (${teams.join(',')})
+      GROUP BY
+        name,
+        b_team
+      HAVING
+        ave IS NOT NULL
+      ORDER BY
+        ave DESC
+    `);
 
-  const title = format("%s打者 主要%s手 打撃成績\n", leagueArg ? leagueList[leagueArg] + '6' : 'NPB12', posFullDic[posArg]);
-  const rows = [];
-  for (const result of results) {
-    const { batter, tm, ab, hit, ave, hr, rbi } = result;
+    const [ posEn ] = Object.entries(posArgDic).find(([, value]) => value == position);
 
-    rows.push(format(
-      '\n%s(%s) %s (%s-%s) %s本 %s打点',
-      batter, tm, trimRateZero(ave), ab, hit, hr, rbi
-    ));
-  }
+    const title = format("%s打者 主要%s手 打撃成績\n", leagueArg ? leagueList[leagueArg] + '6' : 'NPB12', posFullDic[posEn]);
+    const rows = [];
+    for (const result of results) {
+      const { batter, tm, ab, hit, ave, hr, rbi } = result;
 
-  if (getIsTweet()) {
-    await tweetMulti(title, rows);
-  } else {
-    displayResult(title, rows);
+      rows.push(format(
+        '\n%s(%s) %s (%s-%s) %s本 %s打点',
+        batter, tm, trimRateZero(ave), ab, hit, hr, rbi
+      ));
+    }
+
+    if (getIsTweet()) {
+      await tweetMulti(title, rows);
+    } else {
+      displayResult(title, rows);
+    }
   }
 })();
