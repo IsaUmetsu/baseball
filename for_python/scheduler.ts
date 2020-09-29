372511b4f470a172aed589f9a486a330b4e0635f
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-09-29 22:06:10 +0900

    modefied scheduler

--- for_python/scheduler.ts
+++ for_python/scheduler.ts
@@ -8,68 +8,73 @@ import { execBatRc5Team, execMonthStand, execPitchGroundFlyStart, execPitchPerOu
  * 
  */
 const execAfterGame = async () => {
+  const connection = await createConnection('default');
   // 各チーム
   await execBatRc5Team();
   await execPitchRc10Team();
   // 各リーグ
   await execDayBatTeam();
+
+  await connection.close();
 }
 
 /**
  * 
  */
 const execAfterLeftMound = async () => {
+  const connection = await createConnection('default');
+
   await execPitchStrikeSwMsGame();
   await execPitchType();
   await execPitchGroundFlyStart();
   await execPitchPerOut();
+
+  await connection.close();
 }
 
 /**
  * 試合終了前 (ナイトゲーム)
  */
-schedule('5,35 17-18 * 9-11 *', async () => {
-  await createConnection('default');
+schedule('10,40 17-18 * 9-11 *', async () => {
+  const connection = await createConnection('default');
   await execPitchRaPerInningStart();
+  await connection.close();
 });
 
 /**
  * 試合終了前 (土日 デイゲーム)
  */
 schedule('10,40 13-16 * 9-11 0,6', async () => {
-  await createConnection('default');
+  const connection = await createConnection('default');
   await execPitchRaPerInningStart();
+  await connection.close();
 });
 
 /**
  * 試合終了後 (ナイトゲーム)
  */
-schedule('*/15 21-23 * 9-11 *', async () => {
-  await createConnection('default');
+schedule('2,17,32,47 21-23 * 9-11 *', async () => {
   await execAfterGame();
 });
 
 /**
  * 試合終了後 (土日 デイゲーム)
  */
-schedule('*/15 16-18 * 9-11 0,6', async () => {
-  await createConnection('default');
+schedule('2,17,32,47 16-18 * 9-11 0,6', async () => {
   await execAfterGame();
 });
 
 /**
  * 先発投手降板後 (ナイトゲーム)
  */
-schedule('*/15 19-23 * 9-11 *', async () => {
-  await createConnection('default');
+schedule('4,19,34,49 19-23 * 9-11 *', async () => {
   await execAfterLeftMound();
 });
 
 /**
  * 先発投手降板後 (土日 デイゲーム)
  */
-schedule('*/15 13-17 * 9-11 0,6', async () => {
-  await createConnection('default');
+schedule('4,19,34,49 13-17 * 9-11 0,6', async () => {
   await execAfterLeftMound();
 });
 
@@ -77,13 +82,15 @@ schedule('*/15 13-17 * 9-11 0,6', async () => {
  * 毎週日曜日 試合終了後
  */
 schedule('*/15 16-18,21-23 * 9-11 0', async () => {
-  await createConnection('default');
+  const connection = await createConnection('default');
 
   await execWeekStand();
   await execWeekBatChamp();
 
   await execMonthStand();  
   await execMonthBatChamp();
+
+  await connection.close();
 });
 
 /**
@@ -91,9 +98,11 @@ schedule('*/15 16-18,21-23 * 9-11 0', async () => {
  */
 schedule('*/15 16-18,21-23 * 9-11 *', async () => {
   if (moment().add(1, 'days').format('D') == '1') {
-    await createConnection('default');
+    const connection = await createConnection('default');
 
     await execMonthStand();
     await execMonthBatChamp();
+
+    await connection.close();
   }
 });
