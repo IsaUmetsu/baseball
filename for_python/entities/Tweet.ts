6e349ac9941c8826de34bf4697b9abb354bbc911
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-09-30 14:27:56 +0900

    modefied definitil

--- for_python/entities/Tweet.ts
+++ for_python/entities/Tweet.ts
@@ -5,7 +5,7 @@ export class Tweet extends BaseEntity {
   @PrimaryGeneratedColumn({ type: "int", name: "id" })
   id: number;
 
-  @Column("varchar", { name: "script_name", nullable: true, length: 20 })
+  @Column("varchar", { name: "script_name", nullable: true, length: 30 })
   scriptName: string | null;
 
   @Column("varchar", { name: "team", nullable: true, length: 20 })
