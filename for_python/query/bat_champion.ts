24cf731252ad28d8a1e1d4b72f5ab5506761c65b
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-10-02 16:04:00 +0900

    commonaize bat champ

---
+++ for_python/query/bat_champion.ts
@@ -0,0 +1,13 @@
+
+import { createConnection } from 'typeorm';
+import { getIsTweet } from '../tweet/tw_util';
+import { execWeekBatChamp, execMonthBatChamp } from './exec_util';
+
+// Execute
+(async () => {
+  await createConnection('default');
+
+  const { PR, TM, LG, D, M } = process.env;
+  if (PR == 'W') await execWeekBatChamp(getIsTweet(), TM, LG, D);
+  if (PR == 'M') await execMonthBatChamp(getIsTweet(), TM, LG, M);
+})();
