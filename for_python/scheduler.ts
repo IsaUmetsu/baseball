33be6550e65db76d5d5e15efba2f0acd79d22e7b
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-10-02 15:00:41 +0900

    modefied scheduler

--- for_python/scheduler.ts
+++ for_python/scheduler.ts
@@ -6,6 +6,13 @@ import { teamArray } from './constant';
 import { generateConnection } from './db_util';
 import { outputLogStart, outputLogEnd } from './tweet/tw_util';
 
+const BEFORE_GAME_NIGHT       = '10,40 17-18 * 9-11 *';
+const BEFORE_GAME_DAY_HOLIDAY = '10,40 13-16 * 9-11 0,6';
+const AFTER_GAME_NIGHT        = '2,17,32,47 21-23 * 9-11 *';
+const AFTER_GAME_DAY_HOLIDAY  = '2,17,32,47 16-18 * 9-11 0,6';
+const AFTER_LEAVE_MOUND_STARTER_NIGHT       = '4,19,34,49 19-23 * 9-11 *';
+const AFTER_LEAVE_MOUND_STARTER_DAY_HOLIDAY = '4,19,34,49 13-17 * 9-11 0,6';
+
 (async () => {
   // await generateConnection();
 })();
@@ -13,86 +20,60 @@ import { outputLogStart, outputLogEnd } from './tweet/tw_util';
 /**
  * 
  */
-const execAfterGame = async () => {
+const execBeforeGame = async (msg = 'before game') => {
+  outputLogStart(msg);
   await generateConnection();
-
-  // 各チーム
-  await execPitchRc10Team();
-  // 各リーグ
-  await execDayBatTeam();
-  await execBatRc5All();
-  await execOnbaseRc5All();
+  await execPitchRaPerInningStart();  // 12
+  outputLogEnd(msg);
 }
 
 /**
  * 
  */
-const execAfterLeftMound = async () => {
+const execAfterLeftMound = async (msg = 'after leave mound') => {
+  outputLogStart(msg);
   await generateConnection();
-
   // per team
-  await execPitchType();
+  await execPitchType();    // 12
   // all pitchers
-  await execPitchStrikeSwMsGame();
-  await execPitchGroundFlyStart();
-  await execPitchPerOut();
+  await execPitchStrikeSwMsGame();  // 2
+  await execPitchGroundFlyStart();  // 2
+  await execPitchPerOut();          // 1
+  outputLogEnd(msg);
 }
 
 /**
- * 試合前 (ナイトゲーム)
+ * 
  */
-schedule('10,40 17-18 * 9-11 *', async () => {
-  outputLogStart('before game');
+const execAfterGame = async (msg = 'after game') => {
+  outputLogStart(msg);
   await generateConnection();
-  await execPitchRaPerInningStart();
-  outputLogEnd('before game');
-});
+  // 各チーム
+  await execPitchRc10Team();  // 12
+  // 各リーグ
+  await execDayBatTeam();     // 2
+  await execBatRc5All();      // 8
+  await execOnbaseRc5All();   // 8
+  outputLogEnd(msg);
+}
 
-/**
- * 試合前 (土日 デイゲーム)
- */
-schedule('10,40 13-16 * 9-11 0,6', async () => {
-  outputLogStart('before game');
-  await generateConnection();
-  await execPitchRaPerInningStart();
-  outputLogEnd('before game');
-});
+// 試合前 (ナイトゲーム)
+schedule(BEFORE_GAME_NIGHT, async () => await execBeforeGame());
 
-/**
- * 試合終了後 (ナイトゲーム)
- */
-schedule('2,17,32,47 21-23 * 9-11 *', async () => {
-  outputLogStart('after game');
-  await execAfterGame();
-  outputLogEnd('after game');
-});
+// 試合前 (土日 デイゲーム)
+schedule(BEFORE_GAME_DAY_HOLIDAY, async () => await execBeforeGame());
 
-/**
- * 試合終了後 (土日 デイゲーム)
- */
-schedule('2,17,32,47 16-18 * 9-11 0,6', async () => {
-  outputLogStart('after game');
-  await execAfterGame();
-  outputLogEnd('after game');
-});
+// 試合開始直後 (ナイトゲーム)
+schedule(AFTER_GAME_NIGHT, async () => await execAfterGame());
 
-/**
- * 先発投手降板後 (ナイトゲーム)
- */
-schedule('4,19,34,49 19-23 * 9-11 *', async () => {
-  outputLogStart('after leave mound');
-  await execAfterLeftMound();
-  outputLogEnd('after leave mound');
-});
+// 試合開始直後 (土日 デイゲーム)
+schedule(AFTER_GAME_DAY_HOLIDAY, async () => await execAfterGame());
 
-/**
- * 先発投手降板後 (土日 デイゲーム)
- */
-schedule('4,19,34,49 13-17 * 9-11 0,6', async () => {
-  outputLogStart('after leave mound');
-  await execAfterLeftMound();
-  outputLogEnd('after leave mound');
-});
+// 先発投手降板後 (ナイトゲーム)
+schedule(AFTER_LEAVE_MOUND_STARTER_NIGHT, async () => await execAfterLeftMound());
+
+// 先発投手降板後 (土日 デイゲーム)
+schedule(AFTER_LEAVE_MOUND_STARTER_DAY_HOLIDAY, async () => await execAfterLeftMound());
 
 /**
  * 毎週日曜日 試合終了後
@@ -127,10 +108,10 @@ schedule('*/15 16-18,21-23 * 9-11 *', async () => {
     await execMonthBatTitle();
     await execPitchTitle();
     // per team
-    for (const team of Object.keys(teamArray)) {
-      await execMonthBatTitle(true, team);
-      await execPitchTitle(true, team);
-    }
+    // for (const team of Object.keys(teamArray)) {
+    //   await execMonthBatTitle(true, team);
+    //   await execPitchTitle(true, team);
+    // }
 
     outputLogEnd('after game month-end');
   }
