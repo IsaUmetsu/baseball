a5db094890ad3eece165b0a01d246b089b24ead8
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-10-02 17:42:00 +0900

    liveBody_util renamed and moved

--- for_python/util/db.ts
+++ for_python/util/db.ts
@@ -2,7 +2,7 @@ import { getRepository, getManager, getConnection, Connection } from 'typeorm';
 
 import { LiveHeaderJson, LiveBodyJson, PitchInfoJson, TeamInfoJson, BenchMemberInfoType, SavedBallCount } from '../type/jsonType';
 import { GameInfo, LiveHeader, LiveBody, PitchInfo, PitchCourse, PitchDetails, PitcherBatter, TeamInfo, GameOrder, BenchMemberInfo, BenchMaster, BatteryInfo, HomerunInfo } from '../entities';
-import { judgePlateAppearance, judgeAtBat, judgeHit, judgeOnbase, judgeError, judgeFc, judgePlayerChange, judgeIsBall, judgeIsStrike, judgePlusScore, judgePlusOutCount, calcTotalBase, indexOfAnd } from '../liveBody_util';
+import { judgePlateAppearance, judgeAtBat, judgeHit, judgeOnbase, judgeError, judgeFc, judgePlayerChange, judgeIsBall, judgeIsStrike, judgePlusScore, judgePlusOutCount, calcTotalBase, indexOfAnd } from './liveBody';
 import { teamNameFullToIni, TOP, BTM, HM, AW } from '../constant';
 import { createConnection } from 'typeorm';
 
