34d8f7cc2ea57f8511c2bcbd91f4a68a784bfb22
Isamu Umetsu <isamu.umetsu@avinton.com>
2020-11-23 17:51:04 +0900

    modefied scheduler

--- for_python/scheduler.ts
+++ for_python/scheduler.ts
@@ -18,17 +18,11 @@ const DURING_GAME_DAY_HOLIDAY = '7,22,37,52 13-17 * 9-11 0,6';
   // await execBatRc5TeamJs('H', '', false);
 })();
 
-// 試合前 (ナイトゲーム)
+// 試合開始直後 (ナイトゲーム)
 schedule(BEFORE_GAME_NIGHT, async () => await execBeforeGame());
 
-// 試合前 (土日 デイゲーム)
-// schedule(BEFORE_GAME_DAY_HOLIDAY, async () => await execBeforeGame());
-
-// 試合開始直後 (ナイトゲーム)
-schedule(AFTER_GAME_NIGHT, async () => await execAfterGame());
-
 // 試合開始直後 (土日 デイゲーム)
-// schedule(AFTER_GAME_DAY_HOLIDAY, async () => await execAfterGame());
+// schedule(BEFORE_GAME_DAY_HOLIDAY, async () => await execBeforeGame());
 
 // 試合中 (ナイトゲーム)
 schedule(DURING_GAME_NIGHT, async () => await execDuringGame());
@@ -41,6 +35,12 @@ schedule(AFTER_LEAVE_MOUND_STARTER_NIGHT, async () => await execAfterLeftMound()
 
 // 先発投手降板後 (土日 デイゲーム)
 // schedule(AFTER_LEAVE_MOUND_STARTER_DAY_HOLIDAY, async () => await execAfterLeftMound());
+
+// 試合終了後 (ナイトゲーム)
+schedule(AFTER_GAME_NIGHT, async () => await execAfterGame());
+
+// 試合終了後 (土日 デイゲーム)
+// schedule(AFTER_GAME_DAY_HOLIDAY, async () => await execAfterGame());
 
 // 毎週日曜日 試合終了後
 // schedule('*/15 16-18,21-23 * 9-11 0', async () => await execAfterGameSunday());
