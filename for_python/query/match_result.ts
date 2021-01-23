0d7860a08f80646bdd2a4b25858707d46c1a8a82
Isamu Umetsu <isamu.umetsu@avinton.com>
2021-01-23 16:10:54 +0900

    update comment

--- for_python/query/match_result.ts
+++ for_python/query/match_result.ts
@@ -4,6 +4,7 @@ import { getIsTweet } from '../util/tweet';
 import { execResultBatPerPitch, execResultPitchPerBat } from '../util/execute';
 
 // 対戦相手ごとの成績 (好相性、悪相性)
+// NM: 名前、PA: 対戦打席、AV: 基準打率、T: タイプ(G: 好相性、B: 悪相性)
 (async () => {
   await createConnection('default');
   const { K, NM, PA, AV, T } = process.env;
