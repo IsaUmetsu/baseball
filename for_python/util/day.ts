bd119198665f697567f5c2b333e5e5ded0e167f5
Isamu Umetsu <isamu.umetsu@avinton.com>
2021-03-20 20:16:18 +0900

    modefied day util bug

--- for_python/util/day.ts
+++ for_python/util/day.ts
@@ -4,7 +4,7 @@ import { format } from 'util';
 export const getDayInfo = (targetDay: string, seasonEndArg: string) => {
 
     const YEAR = process.env.YEAR ?? moment().format("YYYY");
-    const day = moment(format("%s%s", targetDay), "YYYYMMDD");
+    const day = moment(format("%s%s", YEAR, targetDay), "YYYYMMDD");
     const seasonStart = moment(format("%s%s", YEAR, targetDay), "YYYYMMDD");
     const seasonEnd = moment(format("%s%s", YEAR, seasonEndArg), "YYYYMMDD");
 
